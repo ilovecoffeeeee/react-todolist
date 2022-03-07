85f6dab1e6d0b2fff9c9fe1162f0630ff8f5b148
sun_loves_Q <conconxel@gmail.com>
2022-03-07 23:56:22 +0900

    using recat-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -37,38 +37,87 @@ interface IForm {
     password: string,
 }
 
-function ToDoList() {
-    const { register, handleSubmit, formState:{errors} } = useForm<IForm>({
-        defaultValues: {
-            email:"@naver.com",
-        }
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password1: string;
+    extraError?: string;
+  }
+  
+  function ToDoList() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      setError
+    } = useForm<IForm>({
+      defaultValues: {
+        email: "@naver.com",
+      },
     });
-    const onValid = (data:any) => {
+    const onValid = (data: IForm) => {
+      if(data.password !== data.password1) {
+        setError("password1", {message:"Password are not same"}, {shouldFocus:true})
         console.log(data)
-    }
-    console.log(errors);
+      }
+    //   setError("extraError", {message:"Server Offline."}, )
+    console.log(errors)
+    };
     return (
-        <div>
-            <form onSubmit={handleSubmit(onValid)}>
-                <input
-                    {...register("email", {
-                        required: "Email Required",
-                        minLength: 10,
-                        pattern: {
-                            value:/^[A-Za-z0-9._%+-]+@naver.com$/,
-                            message: "Only naver.com emails allowed",
-                        },
-                    })}
-                placeholder="email"
-                />
-                <span>
-                    {errors?.email?.message}
-                </span>
-                <input {...register("password", {required:"Paasword is requierd!", minLength: 5})}placeholder="Password" />
-                <button>Add</button>
-            </form>
-        </div>
-    )
-};
-
-export default ToDoList;
+      <div>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={handleSubmit(onValid)}
+        >
+          <input
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                message: "Only naver.com emails allowed",
+              },
+            })}
+            placeholder="Email"
+          />
+          <span>{errors?.email?.message}</span>
+          <input
+            {...register("firstName", { required: "write here" })}
+            placeholder="First Name"
+          />
+          <span>{errors?.firstName?.message}</span>
+          <input
+            {...register("lastName", { required: "write here", validate: (value) => value.includes("conxel") ? "no conxel allowed" : true,})}
+            placeholder="Last Name"
+          />
+          <span>{errors?.lastName?.message}</span>
+          <input
+            {...register("username", { required: "write here", minLength: 10 })}
+            placeholder="Username"
+          />
+          <span>{errors?.username?.message}</span>
+          <input
+            {...register("password", { required: "write here", minLength: 5 })}
+            placeholder="Password"
+          />
+          <span>{errors?.password?.message}</span>
+          <input
+            {...register("password1", {
+              required: "Password is required",
+              minLength: {
+                value: 5,
+                message: "Your password is too short.",
+              },
+            })}
+            placeholder="Password1"
+          />
+          <span>{errors?.password1?.message}</span>
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default ToDoList;
